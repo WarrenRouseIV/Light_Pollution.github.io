d8b956c59bbe95430df51acbee6920c421535d0a
Ethan A <armsteadethan@gmail.com>
2019-07-02 17:43:31 -0400

    New js file and change to Interact page
    I added a seperate javascrript file to the js folder specifically for the interaction page so to avoid any mistakes.

---
+++ js/interact.js
@@ -0,0 +1,7 @@
+function setup(){
+    createCanvas(1350,635);
+}
+
+function draw(){
+    background(210);
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-//function setup(){
-//    let myCanvas = createCanvas(800,600);
-//}
-//
-//function draw(){
-//    ellipse(25,25,25,25);
-//}
+
